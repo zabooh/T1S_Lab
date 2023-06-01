362d8a661d2ff35f2c1b20dddbea59386feef793
zabooh <martin.ruppert@microchip.com>
2023-06-01 11:43:54 +0200

    first commit

---
+++ firmware/T1S_lab.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=T1S_lab.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/T1S_lab.X.production.hex
